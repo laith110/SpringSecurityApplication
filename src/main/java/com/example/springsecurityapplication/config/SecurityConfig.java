6ba5c75c9870fa39026a289956015d5cae976065
vasilevaleksei <mrl19_97@mail.ru>
2023-03-30 12:18:49 +0300

    Добавлена авторизация, регистрация, аутентификация с помощью Spring Security Добавлена авторизация,регистрация,аутентификация с помощью Spring Security

--- src/main/java/com/example/springsecurityapplication/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityapplication/config/SecurityConfig.java
@@ -6,10 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +18,7 @@ public class SecurityConfig{
     private final PersonDetailsService personDetailsService;
 
     @Bean
-    public PasswordEncoder getPasswordEncode(){
+    public PasswordEncoder getPasswordEncode() {
         return new BCryptPasswordEncoder();
     }
 
